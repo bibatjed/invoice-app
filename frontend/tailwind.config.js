a8e619eca30b9fda139175667adbbedcaac196da
Jeeed <bibatjed@gmail.com>
2024-02-21 10:04:58 +0800

    Add color and font design system

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custom-purple": "#7C5DFA",
+        "custom-light-purple": "#9277FF",
+        "custom-dark-blue": "#1E2139",
+        "custom-darker-blue": "#252945",
+        "custom-light-grey": "#DFE3FA",
+        "custom-medium-grey": "#888EB0",
+        "custom-blue-grey": "#7E88C3",
+        "custom-black": "#0C0E16",
+        "custom-red": "#EC5757",
+        "custom-lighter-purple": "#9277FF",
+        "custom-white": "#F8F8FB",
+        "custom-darkest": "#141625",
+      },
+    },
   },
   plugins: [],
 };
